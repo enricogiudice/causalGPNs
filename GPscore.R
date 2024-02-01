a144df3fa5b5b0bc91a0bba555b87984d7054ba9
enricogiudice <83815622+enricogiudice@users.noreply.github.com>
2024-02-01 16:10:57 +0100

    Add files via upload

---
+++ GPscore.R
@@ -0,0 +1,20 @@
+usrDAGcorescore <- function (j, parentnodes, n, param) {  # GP Laplace approximate score 
+  lp <- length(parentnodes)  # number of parents
+  scoreconstvec <- param$scoreconstvec
+  y <- param$data[ ,j]
+  switch(as.character(lp),
+         "0" = {  # no parents
+           llik <- Gauss.lap(y)
+           corescore <- scoreconstvec[lp+1] + llik
+         },
+         {  # one or more parents
+           corescore <- scoreconstvec[lp+1]
+           X <- param$data[ ,parentnodes]
+           llik <- GP.lap(y, X)
+           corescore <- corescore + llik
+           if (!is.null(param$logedgepmat)) {  # if there is an additional edge penalization
+             corescore <- corescore - sum(param$logedgepmat[parentnodes, j])
+           }
+         })
+  return(corescore)
+}
